63180878a7a5ba703360c9f5d6d8d7cb4dc4dc8f
Martin Scocco <martin.x.scocco.-nd@disney.com>
2019-06-02 17:18:16 -0300

    change http to https backend request

--- third-love/src/actions/index.js
+++ third-love/src/actions/index.js
@@ -2,7 +2,7 @@ import { FETCH_INFO, SAVE_SWATCH_DATA } from '../constants';
 import axios from 'axios';
 
 export function fetchInfo() {
-  const response = axios.get('http://www.mocky.io/v2/5c6c3a92320000e83bbef971');
+  const response = axios.get('https://www.mocky.io/v2/5c6c3a92320000e83bbef971');
 
   return {
     type: FETCH_INFO,
